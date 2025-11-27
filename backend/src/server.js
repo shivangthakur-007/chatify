335fba43eb64749e4a6144f7cde317b169e89f29
Dance <thakurshivang579@gmail.com>
2025-11-27 19:26:22 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import authRoute from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
 
 dotenv.config();
 
-const app= express();
+const app = express();
+const __dirname = path.resolve();
 
-const PORT= process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use('/api/auth', authRoute);
-app.use('/api/messages', messageRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, ()=> console.log(`Server running on port ${PORT}`));
+app.use("/api/auth", authRoute);
+app.use("/api/messages", messageRouter);
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+}
+
+app.listen(3000, () => console.log(`Server running on port ${PORT}`));
