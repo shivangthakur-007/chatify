98e51229b4123b693bf5e2c40454c0099b1ff7a1
Dance <thakurshivang579@gmail.com>
2025-12-05 15:47:59 +0530

    rate limiting is done

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRouter);
 
 // make ready for deployment
-if (ENV.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "development") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("/*splat", (_, res) => {
